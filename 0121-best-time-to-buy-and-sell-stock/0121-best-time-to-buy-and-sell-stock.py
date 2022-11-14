e68a434098ae191f0e91490f9674ecc1a392b4c9
Dervon Browne <dervonb@gmail.com>
2022-11-14 05:28:18 -0500

    Time: 1023 ms (95.79%), Space: 25 MB (86.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,12 +3,9 @@ class Solution:
         er=0
         b=0
         s=1
-        print(b,s,er)
         while s<len(prices):
-            print(b,s,len(prices))
             if prices[b]>prices[s]:
                 b=s
-                print("skip",b,s,er)
             elif prices[s]-prices[b]>er:
                 er=prices[s]-prices[b]
             s+=1 
