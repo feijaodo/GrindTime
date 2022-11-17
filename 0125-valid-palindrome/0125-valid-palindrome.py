ce23e82b040892e2b35482819e63d3e280459cb1
Dervon Browne <dervonb@gmail.com>
2022-11-17 03:04:21 -0500

    Time: 44 ms (96.51%), Space: 15.3 MB (21.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        aug=0
-        for i,c in enumerate(s,1):
-            index=-i+aug
-            
-            if c.isalnum()==False:
-                aug+=1
-                continue
-            while s[index].isalnum()==False:
-                    index-=1
-                    aug-=1
-                    print(aug,index,c,s[index])
-                    pass
-            #if index*-1>len(s)/2:
-             #   break
-            if c.upper() != s[index].upper():
-                print('comparing',c,s[index])
-                return False
-                
-        return True
-    
-        
+        s=[c for c in s.upper() if c.isalnum()]
+       # print(s[0:int(len(s)/2)+1:1],s[-1:-(int(len(s)/2)+2):-1])
+        if s[0:int(len(s)/2)+1:1]==s[-1:-(int(len(s)/2)+2):-1]:
+            return True
+        return False
