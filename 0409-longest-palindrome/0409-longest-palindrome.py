db391d5e40da1733e9c41803fb3d3d24d420430c
Dervon Browne <dervonb@gmail.com>
2022-11-30 02:08:12 -0500

    Time: 68 ms (33.11%), Space: 13.8 MB (67.20%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,12 +1,7 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        switch=0
-        ineligible=0
-        for l in set(s):
-            print(l)
-            if s.count(l)&1==True:
-                if switch==1:
-                    ineligible+=1
-                else:
-                    switch=1
-        return len(s)-ineligible
+        count=[l for l in set(s) if s.count(l)&1==True]
+        if len(count)>=1:
+            return len(s)-(len(count)-1)
+        else:
+            return len(s)
